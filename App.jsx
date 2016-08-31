2a0cf08f41179a4ad396ef7de2f691bbf4d553f5
deng3h <3hdeng@gmail.com>
2016-08-31 04:24:50 +0000

    reactjs tutorial 1

--- App.jsx
+++ App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 import IconButton from 'material-ui/IconButton';
+
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import * as Colors from 'material-ui/styles/colors';
+const customTheme = {
+  palette: { 
+    primary1Color: Colors.cyan500,
+    primary2Color: Colors.cyan700,
+    primary3Color: Colors.grey400
+  }
+};
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+const theme = getMuiTheme(baseTheme);
 
 
 //export default IconButtonExampleSimple;
@@ -10,18 +23,20 @@ class App extends React.Component {
     render() {
         const IconButtonExampleSimple = () => (
             <div>
-              <IconButton iconClassName="muidocs-icon-custom-github" />
+            <IconButton> <i className="material-icons" style={{color: 'red'}}>exit_to_app</i></IconButton>
+<IconButton iconClassName="material-icons" style={{color: 'red', backgroundColor:'transparent'}} />
+<IconButton iconClassName="muidocs-icon-custom-github" style={{color: 'red', backgroundColor:'transparent'}} />
               <IconButton iconClassName="muidocs-icon-custom-github" disabled={true} />
             </div>
         );
         return (
-            <div>
-            Hello World!!!
-             <MuiThemeProvider>
+             <MuiThemeProvider muiTheme={theme} >
+             <div>
+             this is a test for IconButton in material-ui<br/>
      <IconButtonExampleSimple />
+     </div>
   </MuiThemeProvider>
          
-         </div>
         );
     }
 }
